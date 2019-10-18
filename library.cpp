8a518f3cc80a7c75bad12981e3ff257a7e12ddb4
Merkaber <social.pio@posteo.jp>
2019-10-19 01:08:50 +0200

    Initial commit

---
+++ library.cpp
@@ -0,0 +1,13 @@
+/*
+ * EVEAuth
+ *
+ * Copyright (c) 2019 Simon Piorecki
+ */
+
+#include "library.h"
+
+#include <iostream>
+
+void hello() {
+
+}
---
+++ library.h
@@ -0,0 +1,12 @@
+/*
+ * EVEAuth
+ *
+ * Copyright (c) 2019 Simon Piorecki
+ */
+
+#ifndef EVEAUTH_LIBRARY_H
+#define EVEAUTH_LIBRARY_H
+
+void hello();
+
+#endif //EVEAUTH_LIBRARY_H
