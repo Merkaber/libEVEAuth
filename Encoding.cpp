a7164079bfacba384eb76641c414868b7ad26491
Merkaber <social.pio@posteo.jp>
2019-10-25 14:52:50 +0200

    Added files for class Encoding

---
+++ Encoding.cpp
@@ -0,0 +1,7 @@
+/*
+ * EVEAuth
+ *
+ * Copyright (c) 2019 Simon Piorecki
+ */
+
+#include "Encoding.h"
---
+++ Encoding.h
@@ -0,0 +1,16 @@
+/*
+ * EVEAuth
+ *
+ * Copyright (c) 2019 Simon Piorecki
+ */
+
+#ifndef EVEAUTH_ENCODING_H
+#define EVEAUTH_ENCODING_H
+
+
+class Encoding {
+
+};
+
+
+#endif //EVEAUTH_ENCODING_H
