6973be0008f13fc87cfed5495a1c80daf7aaaaf4
Merkaber <social.pio@posteo.jp>
2019-10-25 23:56:16 +0100

    Changed order of base64Chars

--- encodings/Base64.h
+++ encodings/Base64.h
@@ -19,10 +19,10 @@ namespace EVEAuth {
 
     private:
         static constexpr std::array<char, 64> base64Chars = {
-                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/',
                 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
                 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-                'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+                'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
         };
 
         static constexpr char base64Fill = '=';
