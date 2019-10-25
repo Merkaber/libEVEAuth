79fce069d182f9f95cfaec1fba1a916f140090db
Merkaber <social.pio@posteo.jp>
2019-10-25 16:14:08 +0200

    Added bas64 fill char

--- encodings/Encoding.h
+++ encodings/Encoding.h
@@ -18,5 +18,7 @@ namespace EVEAuth {
                 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
                 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
         };
+
+        static constexpr char base64Fill = '=';
     };
 }
