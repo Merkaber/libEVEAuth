813cf75c2f22235d9415eecfe16aedac8c0283f2
Merkaber <social.pio@posteo.jp>
2019-11-03 04:13:01 +0100

    Changed type of base64Fill from char to string

--- encodings/Base64.h
+++ encodings/Base64.h
@@ -31,7 +31,7 @@ namespace EVEAuth {
                 '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
         };
 
-        static constexpr char base64Fill = '=';
+        inline static const std::string base64Fill = "=";
 
         static constexpr std::array<char, 4> base64UrlSafeChars = {'+', '-', '/', '_'};
 
