4459fe4f0f7d620db4afd20cf6ba08b57c88f0d9
Merkaber <social.pio@posteo.jp>
2019-11-03 14:13:48 +0100

    Added stub sha256 class

---
+++ hashings/SHA256.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Simon on 03.11.2019.
+//
+
+#include "SHA256.h"
---
+++ hashings/SHA256.h
@@ -0,0 +1,14 @@
+//
+// Created by Simon on 03.11.2019.
+//
+
+#ifndef EVEAUTH_SHA256_H
+#define EVEAUTH_SHA256_H
+
+
+class SHA256 {
+
+};
+
+
+#endif //EVEAUTH_SHA256_H
