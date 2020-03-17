7103b8d823e81a75a4dfaeae57d152f5ab2516c0
merkaber <social.pio@posteo.jp>
2020-03-17 16:27:44 +0100

    Added sha256 hash generation

--- Auth.cpp
+++ Auth.cpp
@@ -6,6 +6,47 @@
 
 #include "Auth.h"
 
+/* Includes for sha256 generation */
+#include "iomanip"
+#include "openssl/evp.h"
+
 EVEAuth::Auth::Auth(std::string &client_id) : client_id(std::move(client_id)) {
 
+}
+
+std::string EVEAuth::generate_hash(const std::string& s) noexcept
+{
+    /* Allocates and returns a digest context */
+    EVP_MD_CTX* context = EVP_MD_CTX_new();
+    
+    if (context == nullptr) return "";
+
+    /* Sets up digest context ctx to use a digest type and its standard implementation (nullptr) */
+    int checkInit = EVP_DigestInit_ex(context, EVP_sha256(), nullptr);
+
+    /* checkInit 1 is success, 0 is failure */
+    if (checkInit == 0) return "";
+
+    /* Hashes s.length() bytes of data at s.c_str() into the digest context */
+    int checkUpdate = EVP_DigestUpdate(context, s.c_str(), s.length());
+
+    /* checkUpdate 1 is success, 0 is failure */
+    if (checkUpdate == 0) return "";
+
+    unsigned char hash[EVP_MAX_MD_SIZE];
+    unsigned int lengthOfHash = 0;
+
+    /* Retrieves the digest value from context and places it in hash
+     * Writes the number of bytes written into lengthOfHash */
+    int checkFinal = EVP_DigestFinal_ex(context, hash, &lengthOfHash);
+
+    /* checkFinal 1 is success, 0 is failure */
+    if (checkFinal == 0) return "";
+
+    std::stringstream ss;
+    for (unsigned int i = 0; i < lengthOfHash; ++i) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int) hash[i];
+    }
+
+    return ss.str();
 }
--- Auth.h
+++ Auth.h
@@ -16,4 +16,11 @@ namespace EVEAuth {
     private:
         const std::string client_id;
     };
+
+    /**
+     * Generates the hash value of the given std::string s
+     * @param s The given string
+     * @return The hash value if successfully hashed, otherwise an empty std::string
+     */
+    std::string generate_hash(const std::string& s) noexcept;
 }
