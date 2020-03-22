1a0257dca916af45d2d0ba966348863fc6a3e8e9
merkaber <social.pio@posteo.jp>
2020-03-22 15:27:17 +0100

    Started to implement code challenge implementation

--- Auth.cpp
+++ Auth.cpp
@@ -9,9 +9,36 @@
 /* Includes for sha256 generation */
 #include "iomanip"
 #include "openssl/evp.h"
+#include "encodings/Base64.h"
+
+/* Include for generating code challenge */
+#include <random>
 
 EVEAuth::Auth::Auth(std::string &client_id) noexcept : client_id(std::move(client_id)) {
 
+}
+
+void EVEAuth::Auth::connect() noexcept
+{
+    generate_code_challenge();
+}
+
+void EVEAuth::Auth::generate_code_challenge() noexcept
+{
+    // Generate PKCE_BYTE_NUM of random bytes
+    std::random_device random_device;
+    std::vector<unsigned char> random_data(PKCE_BYTE_NUM);
+    for (unsigned char& i : random_data)
+    {
+        i = static_cast<unsigned char>(random_device());
+    }
+
+    // Encode the raw random bytes to base64-url-safe
+    EVEAuth::Base64 base64(reinterpret_cast<char*> (random_data.data()));
+    std::string encoded_random_bytes = base64.encode_url_safe();
+
+    // Hash the given code challenge with sha256
+    std::string hashed_enc_rand_bytes = EVEAuth::generate_hash(encoded_random_bytes);
 }
 
 std::string EVEAuth::generate_hash(const std::string& s) noexcept
--- Auth.h
+++ Auth.h
@@ -8,10 +8,22 @@
 
 #include "shared.h"
 
+/*
+ * The number of bytes which is necessary for the PKCE protocol
+ * Specification: https://tools.ietf.org/html/rfc7636
+ */
+#define PKCE_BYTE_NUM 32
+
 namespace EVEAuth {
     class Auth {
     public:
         explicit Auth(std::string& client_id) noexcept;
+
+        void connect() noexcept;
+
+    private:
+
+        void generate_code_challenge() noexcept;
 
     private:
         const std::string client_id;
