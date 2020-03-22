997ffbab0cf9f26d031076534fb5d5ff6b3f22f2
merkaber <social.pio@posteo.jp>
2020-03-22 10:43:41 +0100

    Changed names for new naming convention

--- Auth.cpp
+++ Auth.cpp
@@ -22,29 +22,29 @@ std::string EVEAuth::generate_hash(const std::string& s) noexcept
     if (context == nullptr) return "";
 
     /* Sets up digest context ctx to use a digest type and its standard implementation (nullptr) */
-    int checkInit = EVP_DigestInit_ex(context, EVP_sha256(), nullptr);
+    int check_init = EVP_DigestInit_ex(context, EVP_sha256(), nullptr);
 
-    /* checkInit 1 is success, 0 is failure */
-    if (checkInit == 0) return "";
+    /* check_init 1 is success, 0 is failure */
+    if (check_init == 0) return "";
 
     /* Hashes s.length() bytes of data at s.c_str() into the digest context */
-    int checkUpdate = EVP_DigestUpdate(context, s.c_str(), s.length());
+    int check_update = EVP_DigestUpdate(context, s.c_str(), s.length());
 
-    /* checkUpdate 1 is success, 0 is failure */
-    if (checkUpdate == 0) return "";
+    /* check_update 1 is success, 0 is failure */
+    if (check_update == 0) return "";
 
     unsigned char hash[EVP_MAX_MD_SIZE];
-    unsigned int lengthOfHash = 0;
+    unsigned int length_of_hash = 0;
 
     /* Retrieves the digest value from context and places it in hash
-     * Writes the number of bytes written into lengthOfHash */
-    int checkFinal = EVP_DigestFinal_ex(context, hash, &lengthOfHash);
+     * Writes the number of bytes written into length_of_hash */
+    int check_final = EVP_DigestFinal_ex(context, hash, &length_of_hash);
 
-    /* checkFinal 1 is success, 0 is failure */
-    if (checkFinal == 0) return "";
+    /* check_final 1 is success, 0 is failure */
+    if (check_final == 0) return "";
 
     std::stringstream ss;
-    for (unsigned int i = 0; i < lengthOfHash; ++i) {
+    for (unsigned int i = 0; i < length_of_hash; ++i) {
         ss << std::hex << std::setw(2) << std::setfill('0') << (int) hash[i];
     }
 
