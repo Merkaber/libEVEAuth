01718f395fcc65b6471a187daa233547d77d78fe
merkaber <social.pio@posteo.jp>
2020-03-28 12:34:38 +0100

    Added fields and related getter and setter

--- Token.cpp
+++ Token.cpp
@@ -10,3 +10,38 @@ EVEAuth::Token::Token(std::string& access_token) noexcept : access_token(std::mo
 {
 
 }
+
+const std::string& EVEAuth::Token::get_access_token() const noexcept
+{
+    return access_token;
+}
+
+const std::string& EVEAuth::Token::get_token_type() const noexcept
+{
+    return token_type;
+}
+
+void EVEAuth::Token::set_token_type(const std::string& m_token_type) noexcept
+{
+    token_type = m_token_type;
+}
+
+const std::string& EVEAuth::Token::get_refresh_token() const noexcept
+{
+    return refresh_token;
+}
+
+void EVEAuth::Token::set_refresh_token(const std::string& m_refresh_token) noexcept
+{
+    refresh_token = m_refresh_token;
+}
+
+const int& EVEAuth::Token::get_expires_in() const noexcept
+{
+    return expires_in;
+}
+
+void EVEAuth::Token::set_expires_in(const int &m_expires_in) noexcept
+{
+    expires_in = m_expires_in;
+}
--- Token.h
+++ Token.h
@@ -13,8 +13,23 @@ namespace EVEAuth {
     public:
         explicit Token(std::string& access_token) noexcept;
 
+    /* Getter and setter for private fields */
+    public:
+        const std::string& get_access_token() const noexcept;
+
+        const std::string& get_token_type() const noexcept;
+        void set_token_type(const std::string& m_token_type) noexcept;
+
+        const std::string& get_refresh_token() const noexcept;
+        void set_refresh_token(const std::string& m_refresh_token) noexcept;
+
+        const int& get_expires_in() const noexcept;
+        void set_expires_in(const int& m_expires_in) noexcept;
+
     private:
         const std::string access_token = "";
-
+        std::string token_type = "";
+        std::string refresh_token = "";
+        int expires_in = 0;
     };
 }
