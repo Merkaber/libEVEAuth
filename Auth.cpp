8deec30e6ba9189c79c77e65250bf64b49f57905
Merkaber <social.pio@posteo.jp>
2020-04-10 14:18:20 +0200

    Moved curl global init to correct constructor

--- Auth.cpp
+++ Auth.cpp
@@ -27,8 +27,6 @@
 
 EVEAuth::AuthException::AuthException(std::string message, int error_code) noexcept : message(std::move(message)), error_code(error_code)
 {
-    // Handle winsock stuff
-    curl_global_init(CURL_GLOBAL_ALL);
 }
 
 const char* EVEAuth::AuthException::what() const noexcept
@@ -43,6 +41,8 @@ const int& EVEAuth::AuthException::get_error_code() const noexcept
 
 EVEAuth::Auth::Auth(std::string &client_id, std::string& scope_val) noexcept : client_id(std::move(client_id)), scope_val(std::move(scope_val))
 {
+    // Handle winsock stuff
+    curl_global_init(CURL_GLOBAL_ALL);
     token = new EVEAuth::Token();
 }
 
